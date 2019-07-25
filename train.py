22f1608794fefbf07c14de2333fe2de0a9ac51ec
hyeonho1028 <gusgh1510@naver.com>
2019-07-25 12:38:41 +0900

    commit

--- train.py
+++ train.py
@@ -1,16 +1,18 @@
-# excute gawnghan
-print('train 시작')
+import warnings
+warnings.filterwarnings(action='ignore')
 
-
-# excute hyeonho
-print('preprocess 시작')
+print('train 시작\n')
+print('preprocess1 시작\n')
 import preprocess.preprocess
-print('preprocess 완료')
+print('preprocess1 끝\npreprocess2 시작')
+import preprocess.preprocess2
+print('preprocess2 완료\n')
+
 print('createmarch model 실행')
 print('예상시간 : 10분')
 import model.createmarch
 print('createmarch model 완료')
 print('mfmodel model 실행')
 print('예상시간 : 5시간')
-import model.mfmodel
+#import model.mfmodel
 print('train 완료')
